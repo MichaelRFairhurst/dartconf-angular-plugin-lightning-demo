037bb24a0e65ea434eb04eb99e9e51809adfd985
Mike Fairhurst <mfairhurst@google.com>
2018-01-12 16:11:50 -0800

    Clean setup for demo

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,16 @@
+import 'dart:html';
 import 'package:angular/angular.dart';
+import 'dialog_thing.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  templateUrl: 'app_component.html',
+  directives: const []
 )
 class AppComponent {
   var name = 'Angular';
+
+  var items = ['foo', 'bar'];
+
+  void closeWindow(MouseEvent e) {}
 }
